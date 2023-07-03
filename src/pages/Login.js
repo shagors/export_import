dd7250c81a2a2456b4f9eb80b2e400f0531eb6fb
thtshajjad <shagors@users.noreply.github.com>
2023-07-03 16:52:23 +0600

    design login page and make design it

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,75 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex w-full h-screen">
-      <div
-        className="w-full flex items-center justify-center lg:w-1/2"
-        style={{
-          borderRadius: "50px",
-          background: "#e0e0e0",
-          boxShadow: "20px 20px 60px #bebebe, -20px -20px 60px #ffffff",
-        }}>
-        Form
+      <div className="w-full flex items-center justify-center lg:w-1/2 bg-white">
+        <div
+          className="py-16 px-20"
+          style={{
+            borderRadius: "50px",
+            background: "#e0e0e0",
+            boxShadow: "20px 20px 60px #bebebe, -20px -20px 60px #ffffff",
+          }}>
+          <h1 className="text-5xl font-semibold">Welcome Back</h1>
+          <p className="font-medium text-lg text-gray-500 mt-4 text-center">
+            Please Enter Your Details.
+          </p>
+          <div className="mt-8">
+            <div>
+              <label className="text-lg font-semibold">Email</label>
+              <input
+                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                placeholder="Ener your email"
+                type="email"
+              />
+            </div>
+            <div>
+              <label className="text-lg font-semibold">Password</label>
+              <input
+                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                placeholder="Ener your password"
+                type="password"
+              />
+            </div>
+            <div className="mt-6 flex justify-between items-center">
+              <div>
+                <input type="checkbox" id="remeber" />
+                <label className="ml-2 font-medium text-base" htmlFor="remeber">
+                  Remember me!
+                </label>
+              </div>
+              <button className="font-medium text-base text-violet-400">
+                <Link to="/forgotpassword">Forgot Password</Link>
+              </button>
+            </div>
+            <div className="mt-8 flex flex-col gap-y-4">
+              <button className="active:scale-[.98] active:duration-75 hover:scale-[1.03] ease-in-out transition-all py-3 rounded-xl bg-violet-500 text-white text-lg font-bold">
+                Sign in
+              </button>
+              <button className="flex items-center justify-center border-2 border-gray-100 py-3 rounded-xl gap-2 active:scale-[.98] active:duration-75 hover:scale-[1.03] ease-in-out transition-all">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  class="bi bi-google"
+                  viewBox="0 0 16 16">
+                  <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+                </svg>
+                Sign in with Google
+              </button>
+            </div>
+            <div className="mt-8 flex justify-center items-center">
+              <p className="font-medium text-base">Don't have an account?</p>
+              <button className="text-violet-500 text-base font-medium ml-2">
+                <Link to="/signup">Sign Up</Link>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
         <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-bounce" />
--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import ForgotPassword from "../pages/ForgotPassword";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/forgotpassword" element={<ForgotPassword />} />
     </Routes>
   );
 };
