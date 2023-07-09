16ecad2572a0c720a2b6eb14ed90d2bcc25a55f4
thtshajjad <shagors@users.noreply.github.com>
2023-07-09 15:05:03 +0600

    accounts page add and design some input

--- src/pages/Accounts.js
+++ src/pages/Accounts.js
@@ -1,7 +1,10 @@
-import React from "react";
-import ReactDatePicker from "react-datepicker";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import { Link } from "react-router-dom";
 
 const Accounts = () => {
+  const date = new Date().toLocaleDateString();
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <>
       <div>
@@ -9,21 +12,53 @@ const Accounts = () => {
           <h1 className="flex justify-center items-center text-3xl my-4 uppercase text-violet-500 font-medium ">
             Add Accounts Data :
           </h1>
-          <div className="mt-5 flex justify-center items-center">
-            <form className="card w-[600px] bg-base-100 shadow-xl ">
-              <div className="form-control card-body">
-                <div className="input-group">
-                  <select className="select select-bordered">
-                    <option disabled selected>
-                      Pick the propduct
-                    </option>
-                    <option>Attendance check-check</option>
-                    <option>Thermal printer</option>
-                    <option>Dot printer</option>
-                  </select>
+          <div className="mt-5 lg:flex justify-center items-center">
+            <form className="card lg:w-[700px] bg-base-100 shadow-xl">
+              <div className="lg:flex justify-between items-center">
+                <div className="form-control card-body">
+                  <div className="input-group">
+                    <select className="select select-bordered">
+                      <option disabled selected>
+                        Pick the propduct
+                      </option>
+                      <option>Attendance check-check</option>
+                      <option>Thermal printer</option>
+                      <option>Dot printer</option>
+                    </select>
+                  </div>
                 </div>
-                <div></div>
+                <div className="lg:pr-2 text-center flex justify-center items-center">
+                  <p className="pr-2">Date : </p>
+                  <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                  />
+                </div>
               </div>
+              <div className="lg:flex justify-between items-center px-8 mt-4 lg:mt-0">
+                <div className="join">
+                  <input
+                    className="input input-bordered join-item"
+                    placeholder="Quantity"
+                    type="number"
+                  />
+                  <p className="btn join-item rounded-r-full">Pcs</p>
+                </div>
+                <div className="join mt-4 lg:mt-0">
+                  <div>
+                    <div>
+                      <input
+                        className="input input-bordered join-item"
+                        placeholder="Product Model Name"
+                        type="text"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <Link to="" className="flex justify-end items-center mr-7 py-5">
+                <button className="btn btn-secondary btn-sm">Save</button>
+              </Link>
             </form>
           </div>
         </div>
--- src/pages/ExportImport.js
+++ src/pages/ExportImport.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 const ExportImport = () => {
   return (
     <div className="max-h-screen">
-      <h1 className="text-4xl mt-10 text-center font-bold text-violet-500 uppercase tracking-wide">
+      <h1 className="text-xl lg:text-4xl mt-10 text-center font-bold text-violet-500 uppercase tracking-wide">
         Which Mode you want go?
       </h1>
-      <div className="flex justify-around items-center gap-6 mt-20">
+      <div className="lg:flex justify-around items-center gap-6 mt-10 lg:mt-20">
         <div className="w-220 h-40 bg-gradient-to-tr from-violet-500 to-pink-500 rounded flex justify-center items-center">
           <Link to="/accounts">
             <h1 className="text-4xl p-10 text-white font-semibold">Accounts</h1>
           </Link>
         </div>
-        <div className="w-220 h-40 bg-gradient-to-tr from-violet-500 to-pink-500 rounded flex justify-center items-center">
+        <div className="w-220 h-40 bg-gradient-to-tr from-violet-500 to-pink-500 rounded flex justify-center items-center my-10">
           <Link to="/warehouse">
             <h1 className="text-4xl p-10 text-white font-semibold">
               Warehouse
