62472c2e7eca7f9a8cfab40fd79ee8926461d51c
thtshajjad <shagors@users.noreply.github.com>
2023-07-19 14:00:15 +0600

    purchase page design and data collect from server... design this page

--- src/pages/Purchase.js
+++ src/pages/Purchase.js
@@ -1,7 +1,121 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Purchase = () => {
-  return <div>Purchase</div>;
+  const [transportPath, setTransportPath] = useState([]);
+  const [transportCountry, setTransportCountry] = useState([]);
+
+  const [formData, setFormData] = useState([
+    {
+      transportWay: "",
+      countryName: "",
+    },
+  ]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //   getting transport data from server
+    axios
+      .get("http://localhost:5001/transport")
+      .then((res) => setTransportPath(res.data))
+      .catch((error) => setTransportPath(error));
+
+    //   getting transport country data from server
+    axios
+      .get("http://localhost:5001/transport_country")
+      .then((res) => setTransportCountry(res.data))
+      .catch((error) => setTransportCountry(error));
+  }, []);
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  console.log(formData);
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("", formData)
+      .then((res) => {
+        toast.success("Successfully File added to server");
+        navigate("/purchase");
+        console.log(res);
+      })
+      .catch((err) => toast.error(err.sqlMessage));
+  };
+  return (
+    <>
+      <div>
+        <div className="">
+          <h1 className="flex justify-center items-center text-4xl my-4 uppercase text-info font-bold">
+            Add Accounts Data :
+          </h1>
+          <div className="mt-5 lg:flex justify-center items-center">
+            <form
+              className="card lg:w-[700px] bg-base-100 shadow-xl mt-5"
+              onSubmit={formSubmit}>
+              <div className="lg:flex justify-between items-center">
+                <div className="form-control card-body">
+                  <label className="text-center mb-3">
+                    <span className="lebel-text text-lg font-semibold">
+                      Product Shipment Way
+                    </span>
+                  </label>
+                  <div className="input-group  flex lg:flex-none justify-center items-center">
+                    <select
+                      className="select select-info w-full max-w-xs"
+                      id="selectOption"
+                      value={formData.transportWay}
+                      name="productName"
+                      onChange={handleChange}>
+                      <option selected>---- Pick Transport Way ----</option>
+                      {transportPath?.map((product, index) => (
+                        <option key={index}>{product.transportWay}</option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+                <div className="form-control card-body">
+                  <label className="text-center mb-3">
+                    <span className="lebel-text text-lg font-semibold">
+                      Product Shipment Country
+                    </span>
+                  </label>
+                  <div className="input-group  flex lg:flex-none justify-center items-center">
+                    <select
+                      className="select select-info w-full max-w-xs"
+                      id="selectOption"
+                      value={formData.countryName}
+                      name="countryName"
+                      onChange={handleChange}>
+                      <option selected>---- Pick Shipment Country ----</option>
+                      {transportCountry?.map((product, index) => (
+                        <option key={index}>{product.countryName}</option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-end items-center mr-7 py-5">
+                <button
+                  className="btn btn-info font-bold px-8 py-1 text-slate-700"
+                  type="submit">
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Purchase;
