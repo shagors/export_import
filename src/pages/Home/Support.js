a7e63ac19cd79f71ef455f4f889fefc358e342e1
thtshajjad <shagors@users.noreply.github.com>
2023-08-31 10:37:10 +0600

    new sections add and design it

--- src/pages/Home/Support.js
+++ src/pages/Home/Support.js
@@ -1,40 +1,64 @@
 import React from "react";
 
 import test from "../../assets/support.jpg";
+import Account from "../../assets/account.jpg";
+import Warehouse from "../../assets/warehouse.jpg";
+import Admin from "../../assets/admin.jpg";
 
 const Support = () => {
   return (
     <div className="w-full mt-14 mb-11">
       <div className="text-center">
         <h1 className="text-4xl font-semibold text-purple-600">
-          Our Productivity
+          Know About Works
         </h1>
         <div className="flex max-w-xl lg:max-w-7xl mx-auto gap-8 mt-12 group">
           <div className="bg-white/5 shadow-xl group-hover:blur-sm hover:!blur-none hover:translate-[1.2] p-8 rounded-xl hover:bg-lime-200">
-            <img src={test} alt="Test" className="h-72 mx-auto rounded-lg" />
-            <h1 className="uppercase text-xl font-bold mt-2">Product Name</h1>
-            <h1 className="uppercase text-md font-bold mt-2">Product Model</h1>
+            <img
+              src={Account}
+              alt="Test"
+              className="h-56 mx-auto rounded-lg object-cover"
+            />
+            <h1 className="uppercase text-xl font-bold mt-2">Accounts</h1>
             <p className="text-sm leading-7 my-3 font-light opacity-80">
-              Some Details about this products.Some Details about this
-              products.Some Details about this products
+              Accounts manage financial transactions, record income, expenses,
+              and assets. They provide a clear overview of a company's financial
+              health. Account types include assets, liabilities, equity, income,
+              and expenses. Proper accounting ensures accurate financial
+              reporting, tax compliance, and informed decision-making for
+              businesses and individuals.
             </p>
           </div>
           <div className="bg-white/5 shadow-xl group-hover:blur-sm hover:!blur-none hover:translate-[1.2] p-8 rounded-xl hover:bg-lime-200">
-            <img src={test} alt="Test" className="h-72 mx-auto rounded-lg" />
-            <h1 className="uppercase text-xl font-bold mt-2">Product Name</h1>
-            <h1 className="uppercase text-md font-bold mt-2">Product Model</h1>
+            <img
+              src={Warehouse}
+              alt="Test"
+              className="h-56 mx-auto rounded-lg object-cover"
+            />
+            <h1 className="uppercase text-xl font-bold mt-2">Warehouse</h1>
             <p className="text-sm leading-7 my-3 font-light opacity-80">
-              Some Details about this products.Some Details about this
-              products.Some Details about this products
+              Warehouse work involves managing inventory, storing goods, and
+              coordinating shipments. Workers handle receiving, picking,
+              packing, and shipping products. Efficient warehouse operations
+              ensure timely deliveries, minimize stockouts, and optimize space
+              utilization. It's crucial for smooth supply chain management and
+              customer satisfaction in various industries.
             </p>
           </div>
           <div className="bg-white/5 shadow-xl group-hover:blur-sm hover:!blur-none hover:translate-[1.2] p-8 rounded-xl hover:bg-lime-200">
-            <img src={test} alt="Test" className="h-72 mx-auto rounded-lg" />
-            <h1 className="uppercase text-xl font-bold mt-2">Product Name</h1>
-            <h1 className="uppercase text-md font-bold mt-2">Product Model</h1>
+            <img
+              src={Admin}
+              alt="Test"
+              className="h-56 mx-auto rounded-lg object-cover"
+            />
+            <h1 className="uppercase text-xl font-bold mt-2">Admin</h1>
             <p className="text-sm leading-7 my-3 font-light opacity-80">
-              Some Details about this products.Some Details about this
-              products.Some Details about this products
+              An admin dashboard is a web interface that allows administrators
+              to monitor and manage various aspects of a system or application.
+              It provides insights into user activity, data analytics, system
+              health, and configuration settings. Admin dashboards enhance
+              decision-making and streamline operations, leading to improved
+              efficiency and informed strategic choices.
             </p>
           </div>
         </div>
